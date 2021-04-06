b47be237f8548fe6825ac956f618896826c4a7eb
Adrian Tkacz <adrian@biomedical-it.com>
2021-04-06 22:59:27 +0200

    Dividend calculation

--- source_controller/currency_rates.py
+++ source_controller/currency_rates.py
@@ -10,7 +10,9 @@ class FiatCurrencyRates:
 
     @staticmethod
     def get_rate_pln(currency, transaction_date, only_working_days=False):
+        # currency exchange rate on the day before
         date = (transaction_date - datetime.timedelta(days=1)).date()
+
         if only_working_days:
             date = HolidayCalculator.previous_working_day(date)  # last working day
 
@@ -21,17 +23,15 @@ class FiatCurrencyRates:
             # Get data from public NBP API
             nbp_api = NbpApiClient()
             nbp_rate = nbp_api.get_rate(currency, date)
-            if nbp_rate > 0:
-                nbp.insert_rate(currency, date, nbp_rate)  # API OK - update DB
-            else:  # No data from previous day, another trying with last working day
-                before_holiday = HolidayCalculator.previous_working_day((transaction_date - datetime.timedelta(days=1)).date())
+            if nbp_rate is None:
+                # No data from previous day, another trying with last working day
+                before_holiday = HolidayCalculator.previous_working_day(date)
                 nbp_rate = nbp.get_rate(currency, before_holiday)  # Check if rate is in DB?
-                if nbp_rate <= 0:  # No data in DB
-                    nbp_rate = nbp_api.get_rate(currency, before_holiday)  # Check NBP API
-                    if nbp_rate > 0:
-                        nbp.insert_rate(currency, date, nbp_rate)  # API OK - update DB with holiday date
-                    else:
+                if nbp_rate is None:
+                    # No data in DB
+                    nbp_rate = nbp_api.get_rate(currency, before_holiday)
+                    if nbp_rate is None:
                         raise Exception("No NBP rate")
-                else:
-                    nbp.insert_rate(currency, date, nbp_rate)  # DB OK - update DB with holiday date
+            else:
+                nbp.insert_rate(currency, date, nbp_rate)  # DB OK - update DB with holiday date
         return nbp_rate
